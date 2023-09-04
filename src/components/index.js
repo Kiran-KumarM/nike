6b0b415a908a0eac480df76b93e0b4edce666df9
Kiran-KumarM <kiran.kumar.msingh@gmail.com>
2023-09-04 11:46:31 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import Nav from "./Nav";
+import Button from "./Button";
+import ShoeCard from "./ShoeCard";
+import PopularProductCard from "./PopularProductCard";
+import ServiceCard from "./ServiceCard";
+import ReviewCard from "./ReviewCard";
+
+export {
+    Button,
+    Nav,
+    ShoeCard,
+    PopularProductCard,
+    ServiceCard,
+    ReviewCard,
+}
